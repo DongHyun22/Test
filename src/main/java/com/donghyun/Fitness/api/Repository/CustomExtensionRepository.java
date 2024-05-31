fca2ac56d4680b954a7ab9e6b8a6df8575edeea7
김동현 <qhfhdls12@naver.com>
2024-05-31 19:30:29 +0900

    feat: Repository 생성

--- src/main/java/com/donghyun/Fitness/api/Repository/CustomExtensionRepository.java
+++ src/main/java/com/donghyun/Fitness/api/Repository/CustomExtensionRepository.java
@@ -1,9 +1,9 @@
 package com.donghyun.Fitness.api.Repository;
 
-import com.donghyun.Fitness.domain.Test;
+import com.donghyun.Fitness.domain.CustomExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TestRepository extends JpaRepository<Test, Long> {
+public interface CustomExtensionRepository extends JpaRepository<CustomExtension, Long> {
 }
