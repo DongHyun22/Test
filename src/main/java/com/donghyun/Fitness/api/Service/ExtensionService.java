2afb7deab3ed59c55159cc2e8e6763d5705f373d
김동현 <qhfhdls12@naver.com>
2024-05-31 19:31:40 +0900

    feat: Service 생성 ( 전체조회, 커스텀 확장자 생성 구현 )

---
+++ src/main/java/com/donghyun/Fitness/api/Service/ExtensionService.java
@@ -0,0 +1,44 @@
+package com.donghyun.Fitness.api.Service;
+
+import com.donghyun.Fitness.api.Controller.request.CreateCustomExtensionRequest;
+import com.donghyun.Fitness.api.Controller.response.CustomExtensionResponse;
+import com.donghyun.Fitness.api.Controller.response.DefaultExtensionResponse;
+import com.donghyun.Fitness.api.Controller.response.ExtensionsResponse;
+import com.donghyun.Fitness.api.Repository.CustomExtensionRepository;
+import com.donghyun.Fitness.api.Repository.DefaultExtensionRepository;
+import com.donghyun.Fitness.domain.CustomExtension;
+import com.donghyun.Fitness.domain.DefaultExtension;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class ExtensionService {
+    private final DefaultExtensionRepository defaultExtensionRepository;
+    private final CustomExtensionRepository customExtensionRepository;
+
+    public ExtensionsResponse getExtensions() {
+        List<DefaultExtension> list = defaultExtensionRepository.findAll();
+        List<DefaultExtensionResponse> defaultExtensionResponseList = new ArrayList<>();
+        for(DefaultExtension d : list){
+             defaultExtensionResponseList.add(DefaultExtensionResponse.of(d));
+        }
+
+        List<CustomExtension> list1 = customExtensionRepository.findAll();
+        List<CustomExtensionResponse> customExtensionResponseList = new ArrayList<>();
+        for(CustomExtension d : list1){
+            customExtensionResponseList.add(CustomExtensionResponse.of(d));
+        }
+
+        return new ExtensionsResponse(defaultExtensionResponseList, customExtensionResponseList);
+    }
+
+    public void createCustomExtension(CreateCustomExtensionRequest request) {
+        customExtensionRepository.save(request.toEntity());
+    }
+}
