473e7c1bde9147e374610b4ff822a8ff89f09edf
김동현 <qhfhdls12@naver.com>
2024-05-31 19:38:03 +0900

    feat: 삭제하는 커스텀 확장자 요청DTO 이름변경

--- src/main/java/com/donghyun/Fitness/api/Controller/request/RemoveCustomExtensionRequest.java
+++ src/main/java/com/donghyun/Fitness/api/Controller/request/RemoveCustomExtensionRequest.java
@@ -2,7 +2,7 @@ package com.donghyun.Fitness.api.Controller.request;
 
 import jakarta.validation.constraints.NotNull;
 
-public class DeleteCustomExtensionRequest {
+public class RemoveCustomExtensionRequest {
     @NotNull
     private Long customExtensionId;
 }
