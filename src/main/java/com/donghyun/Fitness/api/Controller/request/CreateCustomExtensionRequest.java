673fd8b7008e48766577f10e6c8fdf3aa6db5eac
김동현 <qhfhdls12@naver.com>
2024-06-02 19:50:41 +0900

    feat: 커스텀 확장자 최대 입력 길이 20제한

--- src/main/java/com/donghyun/Fitness/api/Controller/request/CreateCustomExtensionRequest.java
+++ src/main/java/com/donghyun/Fitness/api/Controller/request/CreateCustomExtensionRequest.java
@@ -2,11 +2,13 @@ package com.donghyun.Fitness.api.Controller.request;
 
 import com.donghyun.Fitness.domain.CustomExtension;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateCustomExtensionRequest {
     @NotBlank(message = "확장자를 입력해 주세요")
+    @Size(max = 20, message = "확장자는 최대 20자리까지 입력할 수 있습니다.")
     private String customExtensionName;
 
     public CustomExtension toEntity() {
