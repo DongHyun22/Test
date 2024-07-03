da1c2653285785e2c9eb6b3827f71b7c18c8706e
김동현 <qhfhdls12@naver.com>
2024-07-03 19:50:58 +0900

    feat: 멤버DB의 id를 제외한 속성 이름 변경

--- src/main/java/com/donghyun/Fitness/domain/Member.java
+++ src/main/java/com/donghyun/Fitness/domain/Member.java
@@ -17,16 +17,12 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @Column(name = "member_email")
     private String email;
 
-    @Column(name = "member_password")
     private String password;
 
-    @Column(name = "member_name")
     private String name;
 
-    @Column(name = "member_age")
     private int age;
 
     @Builder
